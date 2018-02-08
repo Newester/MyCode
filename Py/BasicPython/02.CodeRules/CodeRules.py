75082877257a5b704263c83163c76982f75ee135
Newester <2544391722@qq.com>
2018-02-08 23:24:07 +0800

    just update

---
+++ Py/BasicPython/02.CodeRules/CodeRules.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3 
+# 解释器，或者#/usr/bin/python3
+# coding=utf-8 
+# 字符编码，或者 # _*_ coding : utf-8 _*_
+
+#单行注释
+'''
+多行注释
+'''
+"""
+多行注释
+"""
+#关键字
+import keyword
+print(keyword.kwlist)
+#缩进
+if True:
+    print("hello python")
+else:
+    pass
+
+
