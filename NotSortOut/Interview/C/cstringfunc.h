6051d1792a171dfc2c86237e6abf88362940f298
Newester <2544391722@qq.com>
2018-03-28 23:48:25 +0800

    更新

---
+++ NotSortOut/Interview/C/cstringfunc.h
@@ -0,0 +1,45 @@
+#ifndef __CSTRFUN_H__
+#define __CSTRFUN_H__
+
+#include <assert.h>
+#include <stdio.h>
+//strcpy
+char *strcpy(char *dst,const char *src){
+	assert(dst && src);
+	char *res = dst;
+	while('\0' != (*res++ = *src++));
+	return res;
+}
+
+//strlen
+size_t strlen(const char *str){
+	assert(NULL != str);
+	size_t len = 0;
+	while('\0' != *str)
+		len++;
+	return len;
+}
+
+//strcat
+char *strcat(char* dst,const char *src){
+	assert(dst && src);
+	char *res = dst;
+	while('\0' != *dst)
+		dst++;
+	while(dst++ = *src++);
+	return res;
+}
+
+//strcmp
+int strcmp(const char* s1,const char* s2){
+	assert(s1 && s2);
+	while(*s1 == *s2){
+		if('\0' == *s1){
+			return 0;
+		}
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
+}
+#endif
